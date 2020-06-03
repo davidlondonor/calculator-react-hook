3d88e731a1616f167327943a51e9d6f9124875cf
david <davidlucky@gmail.com>
2020-06-02 20:29:04 -0500

    after deploy! let see

---
+++ build/precache-manifest.00927e8c6baaa982cf7b0fc1759aa202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe0b8169df9fb5bf23025a5845471ae",
+    "url": "/calculator-react-hook/index.html"
+  },
+  {
+    "revision": "93f62d252e881ad4e802",
+    "url": "/calculator-react-hook/static/css/main.ff474261.chunk.css"
+  },
+  {
+    "revision": "cfc14c3e8162d53ea4ac",
+    "url": "/calculator-react-hook/static/js/2.de6f676b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-react-hook/static/js/2.de6f676b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93f62d252e881ad4e802",
+    "url": "/calculator-react-hook/static/js/main.df157c7e.chunk.js"
+  },
+  {
+    "revision": "f4a212ab8b70063c29cd",
+    "url": "/calculator-react-hook/static/js/runtime-main.2e98ff07.js"
+  }
+]);
